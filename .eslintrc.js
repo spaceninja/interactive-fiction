4a359ad740ec30f979241cc5aff49a485263aa4d
Scott Vandehey <fake-scott@spaceninja.com>
2022-01-10 18:22:57 -0800

    add vue and stylelint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 13,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'plugin:vue/vue3-recommended', 'prettier'],
   rules: {
     'no-console': 'off',
     'lines-between-class-members': [
@@ -11,4 +11,11 @@ module.exports = {
       { exceptAfterSingleLine: true },
     ],
   },
+  overrides: [
+    {
+      // HACK: https://github.com/vuejs/eslint-plugin-vue/issues/1355
+      files: ['**/*.html'],
+      rules: { 'vue/comment-directive': 'off' },
+    },
+  ],
 };
